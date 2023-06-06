4699d0c8713ed2b0b6a2d466328556799c2549b6
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-06-05 19:09:25 -0500

    Fixed a bug where some user's can have non-enum based colors. They will now group into a Custom enum to prevent breakage.

---
+++ src/AniListNet/Helpers/ProfileColorConverter.cs
@@ -0,0 +1,18 @@
+﻿using AniListNet.Objects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace AniListNet.Helpers;
+
+public class ProfileColorConverter : StringEnumConverter
+{
+    public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        var value = (string) reader.Value!;
+        var enumType = Nullable.GetUnderlyingType(objectType) ?? objectType;
+        var names = Enum.GetNames(enumType);
+
+        // Check if the value matches any defined enum value
+        return names.Contains(value, StringComparer.OrdinalIgnoreCase) ? Enum.Parse(enumType, value, true) : UserProfileColor.Custom;
+    }
+}
--- src/AniListNet/Objects/User/UserOptions.cs
+++ src/AniListNet/Objects/User/UserOptions.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using AniListNet.Helpers;
+using Newtonsoft.Json;
 
 namespace AniListNet.Objects;
 
@@ -6,6 +7,7 @@ public class UserOptions
 {
     [JsonProperty("titleLanguage")] public UserMediaTitleLanguage MediaTitleLanguage { get; set; }
     [JsonProperty("displayAdultContent")] public bool DisplayAdultContent { get; set; }
+    [JsonConverter(typeof(ProfileColorConverter))]
     [JsonProperty("profileColor")] public UserProfileColor ProfileColor { get; set; }
     [JsonProperty("staffNameLanguage")] public UserStaffNameLanguage StaffNameLanguage { get; set; }
 }
